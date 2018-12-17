25f66ea8a8af3a060729b164d7e148aa598c612a
Abhijit Rakas <abhijit.rakas@rtcamp.com>
2018-12-17 15:47:57 +0530

    Set password to empty

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -423,9 +423,8 @@ class FeatureContext implements Context
 		}
 
 		$mysql_query = sprintf(
-			'mysql -h"localhost" -u"%s" -p"%s" -e"SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME=%s"',
+			'mysql -h"localhost" -u"%s" -p"" -e"SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME=%s"',
 			'root',
-			'',
 			$site_info['db_name']
 		);
 
